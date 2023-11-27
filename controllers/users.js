e90e0a4dbdf6767938eaaff79d80251c94d5af76
nkhrystiuk <elhe2013@gmail.com>
2023-11-27 12:31:44 +0200

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,68 +1,64 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
-const generateTokens = require("../helpers/generateToken")
-const  { Conflict, Unauthorized  }  = require("http-errors");
+const generateTokens = require("../helpers/generateToken");
+const { Conflict, Unauthorized } = require("http-errors");
 const jwt = require("jsonwebtoken");
 
-const signup = async(req, res, next) => {
-    try {
-        const { name, email, password } = req.body;
-        const userExists = await User.findOne({ email });
-        if (userExists) {
-            throw new Conflict(`Email ${email} in use`);
-        }
-        const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-        await User.create({ name, email, password: hashPassword});
-
-        res.status(201).json({
-            name,
-            email});
-
-    } catch (error) {
-        next(error);
+const signup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      throw new Conflict(`Email ${email} in use`);
     }
+    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    await User.create({ name, email, password: hashPassword });
+
+    res.status(201).json({
+      name,
+      email,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 // Login User and get him Token for access to some route action
 const login = async (req, res, next) => {
+  try {
+    const { password, email } = req.body;
+    const user = await User.findOne({ email });
     try {
-        const { password, email } = req.body;
-        const user = await User.findOne({email});
-        try {
-            const hashPassword = bcrypt.compare(password, user.password);
-        } catch {
-                return res.status(401).json({message:'Invalid Email or password'});
-        }
-
-
-        const { accessToken, refreshToken } = generateTokens(user);
-        await User.findByIdAndUpdate(user._id, { refreshToken });
-        res.cookie('refreshToken', refreshToken, { httpOnly: true });
-        res.json({ accessToken });
-
-    } catch (error) {
-        next(error);
+      const hashPassword = bcrypt.compare(password, user.password);
+    } catch {
+      return res.status(401).json({ message: "Invalid Email or password" });
     }
+
+    const { accessToken, refreshToken } = generateTokens(user);
+    await User.findByIdAndUpdate(user._id, { refreshToken });
+    res.cookie("refreshToken", refreshToken, { httpOnly: true });
+    res.json({ accessToken });
+  } catch (error) {
+    next(error);
+  }
 };
 const refresh = async (req, res, next) => {
-    console.log("req.user._id",req.user._id)
-    try {
-        const { accessToken, refreshToken } = generateTokens(req.user);
-        await User.findByIdAndUpdate(req.user._id, { refreshToken });
-        res.cookie('refreshToken', refreshToken, { httpOnly: true });
-        res.json({ accessToken });
-
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const { accessToken, refreshToken } = generateTokens(req.user);
+    await User.findByIdAndUpdate(req.user._id, { refreshToken });
+    res.cookie("refreshToken", refreshToken, { httpOnly: true });
+    res.json({ accessToken });
+  } catch (error) {
+    next(error);
+  }
 };
 // Logout User
 const logout = async (req, res) => {
-    const { _id } = req.user;
-    await User.findByIdAndUpdate(_id, { refreshToken: "" }, { new: true });
-    res.status(200).json({
-        message: "User successfully logout",
-    });
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { refreshToken: "" }, { new: true });
+  res.status(200).json({
+    message: "User successfully logout",
+  });
 };
 
-module.exports = {signup, login, refresh, logout};
+module.exports = { signup, login, refresh, logout };
--- middlewares/refreshToken.js
+++ middlewares/refreshToken.js
@@ -1,31 +1,27 @@
-const User = require("../models/user")
+const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const { REFRESH_SECRET_KEY } = process.env;
-const { Unauthorized } = require('http-errors');
+const { Unauthorized } = require("http-errors");
 
 const refreshToken = async (req, _, next) => {
-    console.log("refresh")
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+    const { userId } = await jwt.verify(token, REFRESH_SECRET_KEY);
     try {
-        const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
-        console.log("token: ", token);
-        const { userId } = await jwt.verify(token, REFRESH_SECRET_KEY);
-        try {
+      const user = await User.findById(userId);
 
-            const user = await User.findById(userId);
-
-            if (!user || !user.refreshToken) {
-                throw Unauthorized("Not authorizeds");
-            }
-            req.user = user;
-            console.log(req.user);
-            next();
-        } catch (error) {
-            throw Unauthorized("Not authorized");
-        }
+      if (!user || !user.refreshToken) {
+        throw Unauthorized("Not authorizeds");
+      }
+      req.user = user;
+      next();
     } catch (error) {
-        next(error);
+      throw Unauthorized("Not authorized");
     }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = refreshToken;
